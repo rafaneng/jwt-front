6d6100a6907f176771ddd1c5f81170d28f926daf
Rafael Santos <rafaneng@gmail.com>
2020-11-25 10:24:13 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -8,13 +8,15 @@ submit.addEventListener('click', (e) => {
     let usuario = {}
     usuario.login = login.value;
     usuario.senha = senha.value;
+    
+    console.log(usuario)
 
-    fetch('https://jwt-rafaneng.herokuapp.com/api/usuarios', {
-        method: "POST",
-        headers: { "Content-Type":"application/json"},
-        body: JSON.stringify(usuario)
-    })
-    .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+//     fetch('https://jwt-rafaneng.herokuapp.com/api/usuarios', {
+//         method: "POST",
+//         headers: { "Content-Type":"application/json"},
+//         body: JSON.stringify(usuario)
+//     })
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+//     .catch(err => console.log(err))
 })
